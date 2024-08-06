0141e3c675ec46fde194f086fa948dcfec6335ef
Dev Aghera <devaghera194@gmail.com>
2024-08-06 11:02:42 +0530

    added a migration to add 2 new columns

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -2,8 +2,7 @@ const express = require("express");
 
 const router = express.Router();
 
-const v1APIRoute = require("./v1/index");
 
-router.use("/v1", v1APIRoute);
+// router.use("/v1");
 
 module.exports = router;
